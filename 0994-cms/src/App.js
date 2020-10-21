2628137690f56bf86dce8264198fc9005a70f00e
Abay Reushenov <abay.reushenov@gmail.com>
2020-10-21 14:39:54 +0300

    Second commit 21.10.2020

---
+++ 0994-cms/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Menu from "./components/Menu";
+import {Route, BrowserRouter} from "react-router-dom";
+import Pages from "./pages/Pages";
+import AddPage from "./pages/AddPage";
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <BrowserRouter>
+      <div className="row">
+        <Menu/>
+          <div className="col-9">
+            <Route  exact path="/" render={()=>"CMS"}/>
+            <Route exact path="/pages/" render={()=><Pages/>}/>
+            <Route  path="/pages/addPage" render={()=><AddPage/>}/>
+          </div>
+      </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
