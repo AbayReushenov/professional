f39b0b64282b5685e0f56cfc1b6937a8410075d4
Abay Reushenov <abay.reushenov@gmail.com>
2020-10-26 23:08:51 +0300

    26.10.2020 22:56  Группа 0994
    Домашнее задание:
    Внесены изменения в файлы обработки данных на сервере :
    - router.php
    - Page.php
    - добавлены функции  обработки 
       "getBranch",
       "addBranches"

---
+++ 26-10-20-Branch-PHP/router.php
@@ -0,0 +1,25 @@
+<?php
+  session_start();
+  header("Access-Control-Allow-Origin: *");
+  $uri = explode("/",$_SERVER['REQUEST_URI']);
+  require_once('php/db.php');
+  require_once('php/classes/Page.php');
+  if($uri[1] == "addPage"){
+    exit(Page::addPage($_POST['html'],$_POST['css'],$_POST['js'],$_POST['name'], $_POST['title']));
+  }else if($uri[1]=="getPagesJSON"){
+    exit(Page::getPagesJSON());
+  }else if($uri[1]=="getPageByIdJSON"){
+    exit(Page::getPageByIdJSON($_POST['pageId']));
+  }else if($uri[1]=="editPageById"){
+    exit(Page::editPageById($_POST['pageId'],$_POST['name'],$_POST['title'],$_POST['html'],$_POST['css'],$_POST['js']));
+  }else if($uri[1] == "addBranch"){
+    exit(Page::addBranch($_POST['name'], $_POST['name_rus']));
+  }else if($uri[1]=="getBranches"){
+    exit(Page::getBranches());
+  }else{
+    $page = Page::getPageByName($uri[1]);
+    require_once("public/template.php");
+  }
+  
+
+?>
